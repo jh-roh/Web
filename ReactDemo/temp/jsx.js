403fa0d331d2a5f50638747752265aa323d0cc97
njh8810 <njh8810@naver.com>
2023-05-15 16:51:50 +0900

    reactDemo 샘플 데모  체크인

---
+++ ReactDemo/temp/jsx.js
@@ -0,0 +1,7 @@
+<div className="box">
+    <Title text="hellow world" width={200}></Title>
+    <button onClick={() => {}}>좋아요</button>
+    <a href="/home" style={{marginTop: '10px', color: 'red'}}>
+        홈으로이동
+    </a>
+</div>
