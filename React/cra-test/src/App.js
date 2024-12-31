99640a164e3d826547d8b0b7cd16c59014350497
njh8810 <njh8810@naver.com>
2024-12-31 16:32:16 +0900

    React 관련 Run, Build, server 명령어 등 연습

--- React/cra-test/src/App.js
+++ React/cra-test/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import SmallImg from './smallImage.png'
+import BigImg from './bigImage.jpg'
+
+
 // import data from './data.json'
 
 function App() {
@@ -16,6 +20,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={SmallImg} className="App-logo" alt="logo" />
+        <img src={BigImg} className="App-logo" alt="logo" />
         <button onClick={onClick}>데이터 보여주세요</button>
         <p>hello!</p>
         <p>
